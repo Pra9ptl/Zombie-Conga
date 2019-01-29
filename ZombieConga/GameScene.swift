11591594e440e2a19c7d57e2bab1624f1663dd75
snap <snap@snap!.com>
2019-01-29 11:59:00 -0500

    added background image

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -14,6 +14,17 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         // Set the background color of the app
         self.backgroundColor = SKColor.black;
+        
+        // Add a background image
+        let bg = SKSpriteNode(imageNamed:"background1")
+        // Set position of background to middle of screen
+        bg.position = CGPoint(x: size.width/2, y: size.height/2)
+        
+        // Force the background image to always be at the back
+        bg.zPosition = -1
+
+        // Finally, add the background to the Scene Graph
+        addChild(bg)
     }
     
 }
